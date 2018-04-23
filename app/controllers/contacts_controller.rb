7ae161bfde72b04529eb3288924ff38ce97f5235
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-04-23 16:18:42 -0300

    Aninhando telefones

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -76,6 +76,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :kind_id, :phones, :rmk, adress_attributes: [:street, :city, :state])
+      params.require(:contact).permit(:name, :email, :kind_id, :phones, :rmk, adress_attributes: [:street, :city, :state], phones_attributes: [:id, :phone])
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_one :adress, dependent: :destroy
-  has_many :phone, dependent: :destroy
+  has_many :phones, dependent: :destroy
+  
   accepts_nested_attributes_for :adress
+  accepts_nested_attributes_for :phones
 end
