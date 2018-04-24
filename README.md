d16d29b287bf5e558afd0f9641542873b89acd63
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-04-24 17:04:39 -0300

    Precarregamento dos Assets e Integração JS

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+
+
+Heroku app link.
+https://projectudemy.herokuapp.com/
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( alert.js
+                                                  home.js
+                                                  adresses.js
+                                                  kinds.js
+                                                  phones.js
+                                                  contacts.js
+                                                  home.css
+                                                  adresses.css
+                                                  kinds.css
+                                                  phones.css
+                                                  contacts.css
+                                                  notifyjs/notify.js )
